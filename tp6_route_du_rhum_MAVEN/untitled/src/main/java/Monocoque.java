9f9b73806f30367062a047d77c807a0eb7d36b55
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:25:00 +0100

    Ajout documentation Monocoque

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/Monocoque.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/Monocoque.java
@@ -1,12 +1,32 @@
+/**
+ * La classe <code>Monocoque</code> représente un type spécifique de voilier, dérivé de la classe <code>Voilier</code>.
+ * Un voilier de type monocoque est caractérisé par un design à coque unique.
+ */
 public class Monocoque extends Voilier {
 
-    public Monocoque(String nom, SuivreRoute suivreRoute,String nomSkipper,EnumClasse classe) {
-        super(nom,suivreRoute,nomSkipper,classe);
+    /**
+     * Constructeur de la classe <code>Monocoque</code>.
+     * Ce constructeur permet de créer une instance de voilier monocoque en initialisant les informations
+     * telles que le nom, le suivi de la route, le nom du skipper et la classe du voilier.
+     *
+     * @param nom Le nom du voilier monocoque.
+     * @param suivreRoute L'objet représentant le suivi de la route du voilier.
+     * @param nomSkipper Le nom du skipper du voilier.
+     * @param classe La classe du voilier (Monocoque ou Multicoque).
+     */
+    public Monocoque(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClasse classe) {
+        super(nom, suivreRoute, nomSkipper, classe);
     }
 
-
-
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères du voilier monocoque.
+     * Cette méthode remplace la méthode <code>toString</code> de la classe parent <code>Voilier</code>
+     * pour afficher que le voilier est de type monocoque.
+     *
+     * @return Une chaîne représentant le voilier, indiquant qu'il est un monocoque.
+     */
+    @Override
     public String toString() {
-        return nom+" est monocoque";
+        return nom + " est monocoque";
     }
 }
