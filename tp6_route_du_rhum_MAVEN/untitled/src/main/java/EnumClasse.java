00a525c817a908ac419a2c8c3d35ae4bda80b70d
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:21:24 +0100

    Ajout documentation EnumClasse

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumClasse.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumClasse.java
@@ -1,6 +1,16 @@
-
-
+/**
+ * Cette énumération représente les différentes classes de voiliers.
+ * Elle contient deux valeurs représentant les types de classes de voiliers :
+ * <ul>
+ *     <li><b>EnumMono</b> : Classe pour les voiliers monocoques.</li>
+ *     <li><b>EnumMulti</b> : Classe pour les voiliers multicoques.</li>
+ * </ul>
+ */
 public enum EnumClasse {
+
+    /** Classe représentant un voilier monocoque. */
     EnumMono,
+
+    /** Classe représentant un voilier multicoque. */
     EnumMulti
 }
