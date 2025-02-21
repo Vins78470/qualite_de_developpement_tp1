3b821eb505dd02b852270bcd035abc08fcf5498d
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:26:04 +0100

    Ajout documentation RouteOrthodromie

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/RouteOrthodromie.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/RouteOrthodromie.java
@@ -1,10 +1,16 @@
+/**
+ * La classe <code>RouteOrthodromie</code> implémente l'interface <code>SuivreRoute</code>.
+ * Elle représente une route spécifique, celle de l'orthodromie, et contient une méthode pour la suivre.
+ * La méthode <code>suivreRoute</code> affiche un message indiquant que l'on suit cette route.
+ */
+public class RouteOrthodromie implements SuivreRoute {
 
-
-public class RouteOrthodromie implements SuivreRoute{
-
-    public void suivreRoute(){
-        System.out.println( "Vous suivez la route orthodromie");
-
+    /**
+     * Implémentation de la méthode <code>suivreRoute</code> de l'interface <code>SuivreRoute</code>.
+     * Cette méthode affiche un message indiquant que le voilier suit la route orthodromique.
+     */
+    @Override
+    public void suivreRoute() {
+        System.out.println("Vous suivez la route orthodromie");
     }
-
 }
