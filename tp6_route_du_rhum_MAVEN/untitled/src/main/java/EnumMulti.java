57a229618971f1836333b0b172486a4dc725bd9b
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:22:16 +0100

    Ajout documentation EnumMulti

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumMulti.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumMulti.java
@@ -1,5 +1,21 @@
-
-
+/**
+ * Cette énumération représente les différentes catégories de voiliers multicoques.
+ * Elle contient des valeurs qui correspondent aux classes spécifiques de voiliers multicoques.
+ */
 public enum EnumMulti {
-    Multi50,Ultim, RhumMulti
+
+    /**
+     * Multi 50 : Voilier multicoque de 50 pieds utilisé dans des courses comme la Route du Rhum.
+     */
+    Multi50,
+
+    /**
+     * Ultim : Voilier multicoque utilisé pour les courses en haute mer, comme le Trophée Jules Verne.
+     */
+    Ultim,
+
+    /**
+     * RhumMulti : Voilier multicoque participant à la Route du Rhum.
+     */
+    RhumMulti
 }
