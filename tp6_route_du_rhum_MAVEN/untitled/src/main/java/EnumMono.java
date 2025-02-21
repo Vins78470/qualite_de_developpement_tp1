4aea9d366250bff5040e51cbd2cfd1dfff493e30
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:21:50 +0100

    Ajout documentation EnumMono

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumMono.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/EnumMono.java
@@ -1,4 +1,21 @@
-
+/**
+ * Cette énumération représente les différentes catégories de voiliers monocoques.
+ * Elle contient des valeurs qui correspondent aux classes spécifiques de voiliers monocoques.
+ */
 public enum EnumMono {
-    Class40,Imoca,RhumMono
+
+    /**
+     * Classe 40 : Voilier monocoque de 40 pieds utilisé dans la course au large.
+     */
+    Class40,
+
+    /**
+     * Imoca : Voilier monocoque utilisé dans les courses telles que le Vendée Globe.
+     */
+    Imoca,
+
+    /**
+     * RhumMono : Voilier monocoque participant à la Route du Rhum.
+     */
+    RhumMono
 }
