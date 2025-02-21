16955265ed440317228c3c17914e72af463af3c7
Nathan Lyonnet <vinccarret@gmail.com>
2025-02-21 13:25:43 +0100

    Ajout documentation RouteAlize

--- tp6_route_du_rhum_MAVEN/untitled/src/main/java/RouteAlize.java
+++ tp6_route_du_rhum_MAVEN/untitled/src/main/java/RouteAlize.java
@@ -1,8 +1,16 @@
+/**
+ * La classe <code>RouteAlize</code> implémente l'interface <code>SuivreRoute</code>.
+ * Elle représente une route spécifique, celle des alizés, et contient une méthode pour la suivre.
+ * La méthode <code>suivreRoute</code> affiche un message indiquant que l'on suit cette route.
+ */
 public class RouteAlize implements SuivreRoute {
 
-    public void suivreRoute(){
-        System.out.println( "Vous suivez la route des alizés");
-
+    /**
+     * Implémentation de la méthode <code>suivreRoute</code> de l'interface <code>SuivreRoute</code>.
+     * Cette méthode affiche un message indiquant que le voilier suit la route des alizés.
+     */
+    @Override
+    public void suivreRoute() {
+        System.out.println("Vous suivez la route des alizés");
     }
-
 }
